7f4fd15fba575e6f118a155fd983bcbce468e931
rayhan52ve <sajidrayhan875@gmail.com>
2024-01-16 23:13:41 +0600

    gallery-category-and-album

--- public/frontend/carousel/js/main.js
+++ public/frontend/carousel/js/main.js
@@ -1,40 +1,41 @@
-(function($) {
+(function ($) {
 
 	"use strict";
 
-	var fullHeight = function() {
+	var fullHeight = function () {
 
 		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
+		$(window).resize(function () {
 			$('.js-fullheight').css('height', $(window).height());
 		});
 
 	};
 	fullHeight();
 
-	var carousel = function() {
+	var carousel = function () {
 		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:true,
-	    dots: true,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
+			loop: true,
+			autoplay: true,
+			autoplayTimeout: 11000,
+			margin: 0,
+			animateOut: 'fadeOut',
+			animateIn: 'fadeIn',
+			nav: true,
+			dots: true,
+			autoplayHoverPause: false,
+			items: 1,
+			navText: ["<span class='ion-ios-arrow-back'></span>", "<span class='ion-ios-arrow-forward'></span>"],
+			responsive: {
+				0: {
+					items: 1
+				},
+				600: {
+					items: 1
+				},
+				1000: {
+					items: 1
+				}
+			}
 		});
 
 	};
--- resources/views/Frontend/modules/gallery.blade.php
+++ resources/views/Frontend/modules/gallery.blade.php
@@ -11,7 +11,7 @@
                         ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat
                         ipsum dolorem.</p>
 
-                    <a class="cta-btn" href="contact.html">Available for hire</a>
+                    {{-- <a class="cta-btn" href="contact.html">Available for hire</a> --}}
 
                 </div>
             </div>
@@ -20,170 +20,94 @@
 
     <!-- ======= Gallery Section ======= -->
     <section id="gallery" class="gallery">
-        <div class="container-fluid">
+        <div class="container">
+            <div class="col-md-12">
+                <div class="row gy-5 justify-content-start">
 
-            <div class="row gy-4 justify-content-center">
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-1.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-1.jpg')}}" title="Gallery 1" class="glightbox preview-link"><i
-                                    class="bi bi-arrows-angle-expand"></i></a>
-                            {{-- <a href="{{route('front.gallerySingle')}}" class="details-link"><i class="bi bi-link-45deg"></i></a> --}}
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-2.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-2.jpg')}}" title="Gallery 2" class="glightbox preview-link"><i
-                                    class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-3.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-3.jpg')}}" title="Gallery 3" class="glightbox preview-link"><i
-                                    class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-4.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-4.jpg')}}" title="Gallery 4" class="glightbox preview-link"><i
-                                    class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-5.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-5.jpg')}}" title="Gallery 5" class="glightbox preview-link"><i
-                                    class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-6.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-6.jpg')}}" title="Gallery 6" class="glightbox preview-link"><i
-                                    class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-7.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-7.jpg')}}" title="Gallery 7" class="glightbox preview-link"><i
-                                    class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-8.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-8.jpg')}}" title="Gallery 8"
-                                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-9.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-9.jpg')}}" title="Gallery 9"
-                                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-10.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-10.jpg')}}" title="Gallery 10"
-                                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-11.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-11.jpg')}}" title="Gallery 11"
-                                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-12.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-12.jpg')}}" title="Gallery 12"
-                                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-13.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-13.jpg')}}" title="Gallery 13"
-                                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-14.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-14.jpg')}}" title="Gallery 14"
-                                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-15.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-15.jpg')}}" title="Gallery 15"
-                                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
-                <div class="col-xl-3 col-lg-4 col-md-6">
-                    <div class="gallery-item h-100">
-                        <img src="{{asset('frontend/assets/img/gallery/gallery-16.jpg')}}" class="img-fluid" alt="">
-                        <div class="gallery-links d-flex align-items-center justify-content-center">
-                            <a href="{{asset('frontend/assets/img/gallery/gallery-16.jpg')}}" title="Gallery 16"
-                                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                            <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Gallery Item -->
+                    <h3>Gategory Head</h3>
+                    <div class="col-xl-3 col-lg-4 col-md-6">
+                        <a href="!#">
+                            <div class="gallery-item h-100">
+                                <img src="{{ asset('frontend/assets/img/gallery/gallery-1.jpg') }}" class="img-fluid"
+                                    alt="">
+                                <p class="pt-2">Album Name</p>
+                            </div>
+                        </a>
+                    </div><!-- End Gallery Item -->
 
+                    <div class="col-xl-3 col-lg-4 col-md-6">
+                        <div class="gallery-item h-100">
+                            <img src="{{ asset('frontend/assets/img/gallery/gallery-1.jpg') }}" class="img-fluid"
+                                alt="">
+                            <p class="pt-2">Album Name</p>
+                        </div>
+                    </div><!-- End Gallery Item -->
+
+                    <div class="col-xl-3 col-lg-4 col-md-6">
+                        <div class="gallery-item h-100">
+                            <img src="{{ asset('frontend/assets/img/gallery/gallery-1.jpg') }}" class="img-fluid"
+                                alt="">
+                            <p class="pt-2">Album Name</p>
+                        </div>
+                    </div><!-- End Gallery Item -->
+
+                    <div class="col-xl-3 col-lg-4 col-md-6">
+                        <div class="gallery-item h-100">
+                            <img src="{{ asset('frontend/assets/img/gallery/gallery-1.jpg') }}" class="img-fluid"
+                                alt="">
+                            <p class="pt-2">Album Name</p>
+                        </div>
+                    </div><!-- End Gallery Item -->
+
+                    <div class="col-xl-3 col-lg-4 col-md-6">
+                        <div class="gallery-item h-100">
+                            <img src="{{ asset('frontend/assets/img/gallery/gallery-1.jpg') }}" class="img-fluid"
+                                alt="">
+                            <p class="pt-2">Album Name</p>
+                        </div>
+                    </div><!-- End Gallery Item -->
+                    <h3>Gategory Head</h3>
+                    <div class="col-xl-3 col-lg-4 col-md-6">
+                        <div class="gallery-item h-100">
+                            <img src="{{ asset('frontend/assets/img/gallery/gallery-1.jpg') }}" class="img-fluid"
+                                alt="">
+                            <p class="pt-2">Album Name</p>
+                        </div>
+                    </div><!-- End Gallery Item -->
+
+                    <div class="col-xl-3 col-lg-4 col-md-6">
+                        <div class="gallery-item h-100">
+                            <img src="{{ asset('frontend/assets/img/gallery/gallery-1.jpg') }}" class="img-fluid"
+                                alt="">
+                            <p class="pt-2">Album Name</p>
+                        </div>
+                    </div><!-- End Gallery Item -->
+
+                    <div class="col-xl-3 col-lg-4 col-md-6">
+                        <div class="gallery-item h-100">
+                            <img src="{{ asset('frontend/assets/img/gallery/gallery-1.jpg') }}" class="img-fluid"
+                                alt="">
+                            <p class="pt-2">Album Name</p>
+                        </div>
+                    </div><!-- End Gallery Item -->
+
+                    <div class="col-xl-3 col-lg-4 col-md-6">
+                        <div class="gallery-item h-100">
+                            <img src="{{ asset('frontend/assets/img/gallery/gallery-1.jpg') }}" class="img-fluid"
+                                alt="">
+                            <p class="pt-2">Album Name</p>
+                        </div>
+                    </div><!-- End Gallery Item -->
+
+                    <div class="col-xl-3 col-lg-4 col-md-6">
+                        <div class="gallery-item h-100">
+                            <img src="{{ asset('frontend/assets/img/gallery/gallery-1.jpg') }}" class="img-fluid"
+                                alt="">
+                            <p class="pt-2">Album Name</p>
+                        </div>
+                    </div><!-- End Gallery Item -->
+
+                </div>
             </div>
 
         </div>
