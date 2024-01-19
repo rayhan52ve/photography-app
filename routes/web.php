8e1fe9444871751619379663e33b02a2ea973100
rayhan52ve <sajidrayhan875@gmail.com>
2024-01-19 16:01:39 +0600

    breeze-installed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::get('/', function () {
-//     return view('Frontend.modules.index');
+//     return view('welcome');
 // });
 
 Route::name('front.')->prefix('')->group(function () {
@@ -26,3 +27,16 @@ Route::name('front.')->prefix('')->group(function () {
     // Route::get('/services', [FrontendController::class, 'services'])->name('services');
     Route::get('/contact', [FrontendController::class, 'contact'])->name('contact');
 });
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
